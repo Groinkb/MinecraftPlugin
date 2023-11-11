d30445e07c51082a3f9bcde4911b1aeb91db9199
Groinkb <benjamin@mossu.ch>
2023-11-11 19:14:08 +0100

    add javafile

---
+++ src/main/java/stonk/premierplugin/PremierPlugin.java
@@ -0,0 +1,17 @@
+package stonk.premierplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PremierPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
